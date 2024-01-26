571805b9f92de83fbab039bebb4df04430915b24
WuanBui <118819560+WuanBui@users.noreply.github.com>
2024-01-26 18:55:01 +0700

    LogOut

--- kickstore/components/ProductForm.js
+++ kickstore/components/ProductForm.js
@@ -93,99 +93,84 @@ export default function ProductForm({
     }
   }
 
-  return (
-    <form onSubmit={saveProduct}>
-      <label>Product name</label>
-      <input
-        type="text"
-        placeholder="product name"
-        value={title}
-        onChange={(ev) => setTitle(ev.target.value)}
-      />
-
-      <label>Category</label>
-      <select value={category} onChange={(ev) => setCategory(ev.target.value)}>
-        <option value="">Uncategorized</option>
-        {categories.length > 0 &&
-          categories.map((c) => <option value={c._id}>{c.name}</option>)}
-      </select>
-      {propertiesToFill.length > 0 &&
-        propertiesToFill.map((p) => (
+   return (
+      <form onSubmit={saveProduct}>
+        <label>Product name</label>
+        <input
+          type="text"
+          placeholder="product name"
+          value={title}
+          onChange={ev => setTitle(ev.target.value)}/>
+        <label>Category</label>
+        <select value={category}
+                onChange={ev => setCategory(ev.target.value)}>
+          <option value="">Uncategorized</option>
+          {categories.length > 0 && categories.map(c => (
+            <option value={c._id}>{c.name}</option>
+          ))}
+        </select>
+        {propertiesToFill.length > 0 && propertiesToFill.map(p => (
           <div className="">
-            <label>{p.name[0].toUpperCase() + p.name.substring(1)}</label>
+            <label>{p.name[0].toUpperCase()+p.name.substring(1)}</label>
             <div>
-              <select
-                value={productProperties[p.name]}
-                onChange={(ev) => setProductProp(p.name, ev.target.value)}
+              <select value={productProperties[p.name]}
+                      onChange={ev =>
+                        setProductProp(p.name,ev.target.value)
+                      }
               >
-                {p.values.map((v) => (
+                {p.values.map(v => (
                   <option value={v}>{v}</option>
                 ))}
               </select>
             </div>
           </div>
         ))}
-
-      <label>Photos</label>
-      <div className="mb-2 flex flex-wrap gap-1">
-        <ReactSortable
-          list={images}
-          className="flex flex-wrap gap-1"
-          setList={updateImagesOrder}
-        >
-          {!!images?.length &&
-            images.map((link) => (
-              <div
-                key={link}
-                className="h-24 bg-white p-4 shadow-sm rounded-sm border border-gray-200"
-              >
-                <img src={link} alt="" className="rounded-lg" />
+        <label>
+          Photos
+        </label>
+        <div className="mb-2 flex flex-wrap gap-1">
+          <ReactSortable
+            list={images}
+            className="flex flex-wrap gap-1"
+            setList={updateImagesOrder}>
+            {!!images?.length && images.map(link => (
+              <div key={link} className="h-24 bg-white p-4 shadow-sm rounded-sm border border-gray-200">
+                <img src={link} alt="" className="rounded-lg"/>
               </div>
             ))}
-        </ReactSortable>
-        {isUploading && (
-          <div className="h-24 flex items-center">
-            <Spinner />
-          </div>
-        )}
-        <label className="w-24 h-24 cursor-pointer text-center flex flex-col items-center justify-center text-sm gap-1 text-primary rounded-sm bg-white shadow-sm border border-primary">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
-            />
-          </svg>
-          <div>Add image</div>
-          <input type="file" onChange={uploadImages} className="hidden" />
-        </label>
-      </div>
-
-      <label>Description</label>
-      <textarea
-        placeholder="description"
-        value={description}
-        onChange={(ev) => setDescription(ev.target.value)}
-      />
-
-      <label>Price (USD) </label>
-      <input
-        type="number"
-        placeholder="price"
-        value={price}
-        onChange={(ev) => setPrice(ev.target.value)}
-      />
-
-      <button type="submit" className="btn-primary">
-        Save
-      </button>
-    </form>
+          </ReactSortable>
+          {isUploading && (
+            <div className="h-24 flex items-center">
+              <Spinner />
+            </div>
+          )}
+          <label className="w-24 h-24 cursor-pointer text-center flex flex-col items-center justify-center text-sm gap-1 text-primary rounded-sm bg-white shadow-sm border border-primary">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+            </svg>
+            <div>
+              Add image
+            </div>
+            <input type="file" onChange={uploadImages} className="hidden"/>
+          </label>
+        </div>
+        <label>Description</label>
+        <textarea
+          placeholder="description"
+          value={description}
+          onChange={ev => setDescription(ev.target.value)}
+        />
+        <label>Price (in USD)</label>
+        <input
+          type="number" placeholder="price"
+          value={price}
+          onChange={ev => setPrice(ev.target.value)}
+        />
+        <button
+          type="submit"
+          className="btn-primary">
+          Save
+        </button>
+      </form>
   );
 }
